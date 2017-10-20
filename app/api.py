465527a79d5be29e8889779db48e99f0eb70324b
Paul Davis <pjdavis@gmx.com>
2017-10-20 09:49:26 +0100

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,1 +1,9 @@
+from app.models import Experiment, Enzyme, Reactants, Conditions, db
+from flask_restless import APIManager
+from app import create_app
+
+
+manager = APIManager(create_app(), flask_sqlalchemy_db=db)
+
+manager.create_api(Experiment, methods=['GET', 'POST', 'DELETE'])
 
