8a2f9f630fd3754de956477c251e5fe57f342d54
Paul Davis <pjdavis@gmx.com>
2017-10-22 15:13:35 +0100

    add blueprint

--- app/api.py
+++ app/api.py
@@ -1,6 +1,14 @@
 from app.models import Experiment, Enzyme, Reactants, Conditions, db
 from flask_restless import APIManager
 from app import create_app
+from flask import Blueprint
+
+
+
+hourly_data = Blueprint('experiment', __name__)
+
+
+
 
 
 manager = APIManager(create_app(), flask_sqlalchemy_db=db)
