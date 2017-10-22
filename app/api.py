1240881486bd87ecfd931f1ed2989f697be3a06a
Paul Davis <pjdavis@gmx.com>
2017-10-22 15:26:14 +0100

    update views

--- app/api.py
+++ app/api.py
@@ -1,12 +1,15 @@
 from app.models import Experiment, Enzyme, Reactants, Conditions, db
-from flask import Blueprint
+from flask import Blueprint,jsonify
 
 
 experiment = Blueprint('experiment', __name__)
 
 @experiment.route('/')
-def hourly_data_1():
-    pass
+def experiment():
+    exp = Experiment.query.first()
+    return jsonify ('number'=exp.id)
+
+
 
 
 
