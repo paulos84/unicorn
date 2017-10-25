a5b5983d2f0712287080ee74a98b8b47722fbb45
Paul Davis <pjdavis@gmx.com>
2017-10-25 09:42:15 +0100

    Update create.py

--- create.py
+++ create.py
@@ -1,1 +1,69 @@
+from flask import request, jsonify, Blueprint, render_template
+from api import db, Experiment, Conditions
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField
 
+
+exp_create = Blueprint('create_experiment', __name__)
+
+
+
+class ExperimentForm(FlaskForm):
+    name = StringField('name')
+    notes = StringField('notes')
+    dp3 = FloatField('dp3')
+    gos = FloatField('gos')
+    graph_loc = StringField('graph_loc')
+
+
+
+
+#@catalog.route('/product-create', methods=['POST',])
+#def create_product():
+@exp_create.route('/exp-create', methods=['POST', ])
+def create_exp():
+    name = request.form.get('name')
+    price = request.form.get('price')
+    product = Product(name, price)
+    db.session.add(product)
+    db.session.commit()
+    return 'Product created.'
+
+
+
+@catalog.route('/form', methods=['GET', 'POST'])
+def product_form():
+    form = ProductForm()
+
+    if form.validate_on_submit():
+        return 'The username is {}. The password is {}'.format (form.username.data, form.password.data)
+    return render_template('form.html', form=form)
+
+"""
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+
+
+@catalog.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return 'The username is {}. The password is {}'.format (form.username.data, form.password.data)
+    return render_template('form.html', form=form)
+
+
+
+@catalog.route('/products')
+def products():
+    products = Product.query.all()
+    res = {}
+    for product in products:
+        res[product.id] = {
+            'name': product.name,
+            'price': str(product.price)
+                            }
+    return jsonify(res)
+
+"""
