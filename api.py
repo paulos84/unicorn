d72fb367ac8ee088d1e8a6bc06ee62b36da45786
Paul Davis <pjdavis@gmx.com>
2017-10-26 14:31:28 +0100

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 from flask_migrate import Manager, Migrate, MigrateCommand
-
+from create import exp_create
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
@@ -11,10 +11,10 @@ db = SQLAlchemy(app)
 
 class Experiment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(50), nullable=False)
     notes = db.Column(db.String(500))
-    dp3 = db.Column(db.Float(200))
-    gos = db.Column(db.Float(200))
+    dp3 = db.Column(db.Float(200), nullable=False)
+    gos = db.Column(db.Float(200), nullable=False)
     graph_loc = db.Column(db.String(200))
     cond = db.Column(db.Integer, db.ForeignKey('conditions.id'))
 # hold just single peak dp3/gos instead of sequence - allow query - use int or float?
@@ -23,9 +23,7 @@ class Experiment(db.Model):
 class Conditions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temp = db.Column(db.Float(10), nullable=False)
-    pH = db.Column(db.Float(10))
     enz_dose = db.Column(db.Float(20), nullable=False)
-    misc = db.Column(db.String(200), nullable=False)
     exp = db.relationship('Experiment', backref='conditions', lazy='dynamic')
 
 
@@ -38,6 +36,7 @@ migrate = Migrate(app, db)
 db_manager = Manager(app)
 db_manager.add_command('db', MigrateCommand)
 
+app.register_blueprint(exp_create)
 
 if __name__ == '__main__':
     app.run(port=8080)
