fa3f8543fb454086dd3b5a1e2968051518552bae
Paul Davis <pjdavis@gmx.com>
2017-11-08 09:35:11 +0000

    Update api.py

--- api.py
+++ api.py
@@ -15,7 +15,7 @@ app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 
 
-class Experiment(db.Model)
+class Experiment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
@@ -24,74 +24,73 @@ class Experiment(db.Model)
     results_id = db.Column(db.Integer, db.ForeignKey('results.id'))
 
 
-class Conditions(db.Model)
+class Conditions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temp = db.Column(db.Float(50), nullable=False)
     enz_dose = db.Column(db.Float(50), nullable=False)
-    lac = db.Column(db.Float(50), nullable=False)
-    h20 = db.Column(db.Float(50), nullable=False)
-    glu = db.Column(db.Float(50))
-    gal = db.Column(db.Float(50))
-    description = db.Column(db.String(500))
+    lactose = db.Column(db.Float(50), nullable=False)
+    water = db.Column(db.Float(50), nullable=False)
+    glucose = db.Column(db.Float(50))
+    description = db.Column(db.String(800))
     exp = db.relationship('Experiment', backref='conditions', lazy='dynamic')
 
 
-class Results(db.Model)
+class Results(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    times = db.Column(db.String(200))
-    dp3 = db.Column(db.String(200))
-    dp2 = db.Column(db.String(200))
-    glu = db.Column(db.String(200))
-    gal = db.Column(db.String(200))
-    dp2_split = db.Column(db.String(200))
+    time = db.Column(db.Float(50))
+    dp3plus = db.Column(db.Float(50))
+    dp2 = db.Column(db.Float(50))
+    glu = db.Column(db.Float(50))
+    gal = db.Column(db.Float(50))
+    dp2split = db.Column(db.Float(50))
 
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-# default endpoint 127.0.0.18080apiexperiment
+# default endpoint: 127.0.0.1:8080/api/experiment
 manager.create_api(Experiment, methods=['GET', 'POST', 'PUT', 'DELETE'])
 manager.create_api(Conditions, methods=['GET', 'POST', 'PUT', 'DELETE'])
 manager.create_api(Results, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 
-class ExperimentForm(FlaskForm)
-    name = StringField('Experiment name', validators=[InputRequired('Experiment name is required')])
+class ExperimentForm(FlaskForm):
+    name = StringField('Experiment name', default='Exp', validators=[InputRequired('Experiment name is required')])
     date = StringField('Date of experiment', validators=[InputRequired('Date format e.g. 2017-10-01')])
     notes = StringField('Notes on aim, summary etc.')
-    temp = FloatField(Temp ('C), validators=[InputRequired('Temperature value required')])
-    enz_dose = FloatField('Enzyme dose (g)', validators=[InputRequired('Enzyme dose required')])
-    lac = FloatField('lactose monohydrate (g)', validators=[InputRequired('Lactose amount required')])
-    h20 = FloatField('water (g)', validators=[InputRequired('Water amount required')])
-    glu = FloatField('glucose (g)')
-    gal = FloatField('galactose (g)')                    
-    description = StringField(label='Notes relating to conditions', default='404g lactose, 225.6g water')
+    temp = FloatField("Temp ('C)", validators=[InputRequired('Temperature value required')])
+    enz = FloatField('Enzyme dose (g)', validators=[InputRequired('Enzyme dose required')])
+    lac = FloatField('Lactose monohydrate (g)', default='404', validators=[InputRequired('Lactose amount required')])
+    h2o = FloatField('Water (g)', default='225.6', validators=[InputRequired('Water amount required')])
+    glu = FloatField('Glucose (g)')
+    desc = StringField('Notes relating to conditions')
     file = FileField('Results csv file', validators=[FileRequired(), FileAllowed(['csv'], 'csv files only')])
 
 
-def add_results(filename)
-    df = pd.read_csv('uploads{}'.format(filename))
-    labels = ['times', 'dp3+', 'dp2', 'glu', 'gal','dp2_split']
-    results_dict = {a [','.join([str(b) for b in df[a]])][0] for a in labels}
-    results = Results(results_dict)
+def add_results(filename):
+    df = pd.read_csv('uploads/{}'.format(filename))
+    labels = ['time', 'dp3plus', 'dp2', 'glu', 'dp2split']
+    #call .split() on labels in case whitespace surrounding in results csv file??
+    results_dict = {a: [','.join([str(b) for b in df[a]])][0] for a in labels}
+    results = Results(**results_dict)
     db.session.add(results)
     db.session.commit()
     return results.id
 
 
-@app.route('apicreate', methods=['GET', 'POST'])
-def create_exp()
+@app.route('/api/create', methods=['GET', 'POST'])
+def create_exp():
     form = ExperimentForm()
-    if form.validate_on_submit()
-        data = {key form.data[key] for key in form.data if key in ('name', 'date', 'notes')}
-        #NEED TO UPDATE FOLLOWING LINE:
-        cond = {key form.data[key] for key in form.data if key in ('temp', 'enz_dose', 'misc')}
+    if form.validate_on_submit():
+        data = {key: form.data[key] for key in form.data if key in ('name', 'date', 'notes')}
+        cond = {key: form.data[key] for key in form.data if key in ('temp', 'enz', 'lac', 'h2o', 'glu', 'desc')}
         filename = secure_filename(form.file.data.filename)
-        form.file.data.save('uploads' + filename)
+        form.file.data.save('uploads/' + filename)
         results_id = add_results(filename)
-        data['results'] = {'id' results_id}
-        url = 'http127.0.0.18080apiexperiment'
-        qs = Conditions.query.filter_by(temp=cond['temp'], enz_dose=cond['enz_dose'], misc=cond['misc']).first()
-        if qs
-            data['conditions'] = {'id' qs.id}
+        data['results'] = {'id': results_id}
+        url = 'http://127.0.0.1:8080/api/experiment'
+        qs = Conditions.query.filter_by(temp=cond['temp'], enz_dose=cond['enz'], lactose=cond['lac'], water=cond['h2o'],
+                                        glucose=cond['glu'], description=cond['desc']).first()
+        if qs:
+            data['conditions'] = {'id': qs.id}
             requests.post(url, json=data)
             return jsonify(data)
         data['conditions'] = cond
@@ -100,5 +99,5 @@ def create_exp()
     return render_template('exp_form.html', form=form)
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(port=8080, threaded=True)
