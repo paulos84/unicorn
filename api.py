76e34302bcc51214c71fde9a7f431194618c7751
Paul Davis <pjdavis@gmx.com>
2017-11-11 11:19:59 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, SelectField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
@@ -9,10 +9,27 @@ from flask_restless import APIManager
 import requests
 import pandas as pd
 
+# To Do - login password protection  - how to do with Flask-Restful/restless?
+# Datefield in wtforms to ensure date format 2017-10-16
+# Validator to check that enz dose is one d.p
+# remove unused relationships?
+# produce schema on paper showing relationships
+# see  one to many and one-to-one  pp vidz
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
+
+
+class Enzyme(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    enzyme = db.Column(db.String(50), nullable=False)
+    dose = db.Column(db.Float(50), nullable=False)
+    exp = db.relationship('Experiment', backref='owner', lazy='dynamic')
+    # backref means creating virtual column in the class specified in the string e.g. the Experiment class/table
+    #  so that by referencing experiment.owner can see who the owner is
+    # lazy allows find out all the experiments that the Enzyme instance has by saying e.g. enzyme1.experiments by
+    # e.g. for i in enzyme1: print(i.name) - gives list of experiment names
 
 
 class Experiment(db.Model):
@@ -20,7 +37,8 @@ class Experiment(db.Model):
     name = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
     notes = db.Column(db.String(800))
-    conditions_ = db.Column(db.Integer, db.ForeignKey('conditions.id'))
+    enzyme_id = db.Column(db.Integer, db.ForeignKey('enzyme.id'))
+    conditions_id = db.Column(db.Integer, db.ForeignKey('conditions.id'))
     results_id = db.Column(db.Integer, db.ForeignKey('results.id'))
 
 
@@ -32,13 +50,6 @@ class Conditions(db.Model):
     glucose = db.Column(db.Float(50))
     description = db.Column(db.String(800))
     exp = db.relationship('Experiment', backref='conditions', lazy='dynamic')
-    
-    
-class Enzyme(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    batch = db.Column(db.String(50), nullable=False)
-    dose = db.Column(db.Float(50), nullable=False)
 
 
 class Results(db.Model):
@@ -50,6 +61,8 @@ class Results(db.Model):
     gal = db.Column(db.String(200))
     dp2split = db.Column(db.String(200))
 
+
+exp = db.relationship('Experiment', backref='conditions', lazy='dynamic')
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
 # default endpoint: 127.0.0.1:8080/api/experiment
@@ -63,7 +76,9 @@ class ExperimentForm(FlaskForm):
     date = StringField('Date of experiment', validators=[InputRequired('Date format e.g. 2017-10-01')])
     notes = StringField('Notes on aim, summary etc.')
     temp = FloatField("Temp ('C)", validators=[InputRequired('Temperature value required')])
-    enz = FloatField('Enzyme dose (g)', validators=[InputRequired('Enzyme dose required')])
+                        # To do: make way to add new dose or select from choices
+    enz = SelectField('Enzyme', validators=[InputRequired('Enzyme dose required')])
+    dose = FloatField('Enzyme dose (mg/g)', validators=[InputRequired('Enzyme dose required')])
     lac = FloatField('Lactose monohydrate (g)', default='404', validators=[InputRequired('Lactose amount required')])
     h2o = FloatField('Water (g)', default='225.6', validators=[InputRequired('Water amount required')])
     glu = FloatField('Glucose (g)')
@@ -93,7 +108,7 @@ def create_exp():
         results_id = add_results(filename)
         data['results'] = {'id': results_id}
         url = 'http://127.0.0.1:8080/api/experiment'
-        qs = Conditions.query.filter_by(temp=cond['temp'], enz_dose=cond['enz'], lactose=cond['lac'], water=cond['h2o'],
+        qs = Conditions.query.filter_by(temp=cond['temp'], enzyme=cond['enz'], lactose=cond['lac'], water=cond['h2o'],
                                         glucose=cond['glu'], description=cond['desc']).first()
         if qs:
             data['conditions'] = {'id': qs.id}
