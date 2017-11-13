4105da58d9b22688ed07ad420454ccf70def814f
Paul Davis <pjdavis@gmx.com>
2017-11-13 12:36:54 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ class DevelopmentConfig:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_ECHO = False
     SECRET_KEY = 'brownunicorn'
 
