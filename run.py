5540ee71c7f9d0ab13c0581cc4e9811dc3445389
Paul Davis <pjdavis@gmx.com>
2017-11-18 12:10:29 +0100

    update app

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from flask import make_response
 from app.charts import charts
 from app.exp_form import exp_form
 from flask import request
@@ -9,19 +10,21 @@ app.register_blueprint(exp_form)
 app.register_blueprint(charts)
 
 
+def check_credentials(**kwargs):
+    if request.authorization and request.authorization.username == 'admin' and request.authorization.password == 'kong':
+        return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 with app.app_context():
     db.init_app(app)
 
     access_password = 'mysecretkey'
 
-    def check_credentials(**kwargs):
-        if request.headers.get('X-Secret-Key', '') != access_password:
-            raise ProcessingException(code=401)  # Unauthorized
+
 
     manager = APIManager(app, flask_sqlalchemy_db=db)
     http_methods = ['GET', 'POST', 'PUT', 'DELETE']
-    protected = ['GET_SINGLE', 'GET_MANY', 'POST', 'PUT_SINGLE', 'PUT-MANY', 'DELETE_SINGLE', 'DELETE_MANY']
+    protected = ['POST', 'PUT_SINGLE', 'PUT-MANY', 'DELETE_SINGLE', 'DELETE_MANY']
     enz_api = manager.create_api(Enzyme, methods=http_methods,
                                            preprocessors={a: [check_credentials] for a in protected})
     method_api = manager.create_api(Method, methods=http_methods,
