9ca099979b98e11a85cf3ae551e348e879b57051
Paul Davis <pjdavis@gmx.com>
2017-11-21 15:08:12 +0000

    Update api_v2.py

--- api_v2.py
+++ api_v2.py
@@ -132,15 +132,15 @@ def plot(exp_id, chart_id='chart_ID', chart_type='line', chart_height=550, chart
                            yAxis=yaxis)
 
 
+# move following to a blueprint if not depend upon app or db instance
+# Function to reprocess results to account for any non-lactose starting material
+# Make so that can save the csv onto local computer
 class ConvertResultsForm(FlaskForm):
     file = FileField('Results csv file', validators=[FileRequired(), FileAllowed(['csv'], 'csv files only')])
 
 
-# move following to a blueprint if not depend upon app or db instance
-# Function to reprocess results to account for any non-lactose starting material
-# Make so that can save the csv onto local computer 
 @app.route('/convert-results', methods=['GET', 'POST'])
-def convert_results(exp_id):
+def convert_results():
     if request.authorization and request.authorization.username == 'admin' and request.authorization.password == 'kong':
         upload_form = ConvertResultsForm()
         if upload_form.validate_on_submit():
@@ -148,7 +148,6 @@ def convert_results(exp_id):
             upload_form.file.data.save('processed_csv/' + filename)
             df = pd.read_csv('processed_csv/{}'.format(filename))
             df.columns = df.columns.str.strip()
-            labels = ['']
             return 'file created'
         return render_template('convert_results.html', form=upload_form)
     return make_response('Unable to verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
