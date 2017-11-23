2bebe5106961314471558641c756c1533bbbb0e9
Paul Davis <pjdavis@gmx.com>
2017-11-23 10:32:12 +0000

    Update csv_upload.py

--- csv_upload.py
+++ csv_upload.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, request, make_response
 from flask_wtf import FlaskForm
-from wtforms import FloatField
+from wtforms import FloatField, StringField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import InputRequired
 from werkzeug.utils import secure_filename
@@ -15,6 +15,7 @@ process_csv = Blueprint('hourly', __name__)
 class ConvertResultsForm(FlaskForm):
     lactose = FloatField('Lactose monohydrate (g)', default=404, validators=[InputRequired('Lactose amount required')])
     glucose = FloatField('Glucose (g)', validators=[InputRequired('Water amount required')])
+    email = StringField('Email address')
     file = FileField('Results csv file', validators=[FileRequired(), FileAllowed(['csv'], 'csv files only')])
 
 
@@ -23,11 +24,13 @@ def convert_results():
     if request.authorization and request.authorization.username == 'admin' and request.authorization.password == 'kong':
         upload_form = ConvertResultsForm()
         if upload_form.validate_on_submit():
-            glu_conc = upload_form.data['glucose'] / (upload_form.data['lactose'] * 0.95 + upload_form.data['glucose'])
+            added_glu = upload_form.data['glucose'] / (upload_form.data['lactose'] * 0.95 + upload_form.data['glucose'])
             filename = 'preprocessed_csv_' + secure_filename(upload_form.file.data.filename)
             upload_form.file.data.save('processed_csv/' + filename)
             df = pd.read_csv('processed_csv/{}'.format(filename))
-            df.columns = df.columns.str.strip()
-            return 'Results csv file created to account for glucose as {}% of total solids'.format(str(glu_conc*100))
+            df.iloc[6, 1:] = df.iloc[6, 1:] - added_glu
+            df.iloc[:, 1:] = df.iloc[:, 1:] / df.iloc[:, 1:].sum() * 100
+            df.to_csv('minus_glu.csv'.format(upload_form.file.data.filename), sep=',')
+            return 'Results csv file created to account for glucose as {}% of total solids'.format(str(added_glu*100))
         return render_template('convert_results.html', form=upload_form)
     return make_response('Unable to verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
